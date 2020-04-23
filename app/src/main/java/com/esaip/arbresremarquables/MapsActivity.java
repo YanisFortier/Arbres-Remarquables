31265e0e8735986fef1d4d68b8c0600af5cb63cc
Yanis <yfortier.ir2021@esaip.org>
2020-04-23 09:40:07 +0200

    MAJ - Changement icon
    Signed-off-by: Yanis <yfortier.ir2021@esaip.org>

--- app/src/main/java/com/esaip/arbresremarquables/MapsActivity.java
+++ app/src/main/java/com/esaip/arbresremarquables/MapsActivity.java
@@ -22,13 +22,15 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import ru.dimorinny.floatingtextbutton.FloatingTextButton;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    //Variables static
     private static LatLngBounds FranceMetroBounds = new LatLngBounds(
             new LatLng(42.6965954131, -4.32784220122),
             new LatLng(50.4644483399, 7.38468690323)
     );
 
-    private GoogleMap mMap;
+
     //Location
+    private GoogleMap mMap;
     private Location mCurrentLocation;
     private FusedLocationProviderClient fusedLocationProviderClient;
 
@@ -37,17 +39,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        //Location
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
 
         //Boutons
         FloatingTextButton btnArbre = findViewById(R.id.floatingTxtBtnArbre);
         FloatingTextButton btnAlignement = findViewById(R.id.floatingTxtBtnAlignement);
         FloatingTextButton btnEspaceBoise = findViewById(R.id.floatingTxtBtnEspaceBoise);
+
         // Permission
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+        } else {
+            //Location
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         }
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
@@ -90,22 +96,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(FranceMetroBounds, 150), 2000, null);
             }
         });
-
-
-    }
-
-    public void ajouterArbre(View view) {
-        Intent intent = new Intent(this, AjoutArbre.class);
-        startActivity(intent);
-    }
-
-    public void ajouterAlignement(View view) {
-        Intent intent = new Intent(this, AjoutAlignement.class);
-        startActivity(intent);
-    }
-
-    public void ajouterEspaceBoise(View view) {
-        Intent intent = new Intent(this, AjoutEspaceBoise.class);
-        startActivity(intent);
     }
 }
