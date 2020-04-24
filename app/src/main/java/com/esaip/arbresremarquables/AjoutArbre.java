b15b641029b19cb6ae026dac80cbcef3f130fd57
doreaumaxime <mdoreau.ir2021@esaip.org>
2020-04-24 16:27:56 +0200

    Création Formulaire Arbre v2

--- app/src/main/java/com/esaip/arbresremarquables/AjoutArbre.java
+++ app/src/main/java/com/esaip/arbresremarquables/AjoutArbre.java
@@ -1,6 +1,7 @@
 package com.esaip.arbresremarquables;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
@@ -21,9 +22,15 @@ import com.google.android.gms.tasks.Task;
 import static android.widget.AdapterView.OnItemSelectedListener;
 
 public class AjoutArbre extends AppCompatActivity {
+
+    public static final String SHARED_PREFS = "SHARED_PREFS";
+    public static final String TEXT_NOM_PRENOM = "NOM_PRENOM";
+    public static final String TEXT_ADRESSE_MAIL = "ADRESSE_MAIL";
+    public static final String TEXT_PSEUDO = "PSEUDO";
+
     //Variables
     private Spinner spinnerNomArbre;
-    private EditText editTextLatitude, editTextLongitude;
+    private EditText editTextLatitude, editTextLongitude, editTextNomPrenom, editTextAdresseMail, editTextPseudo;
     private LinearLayout editTextNomArbre;
 
     //Location
@@ -40,7 +47,9 @@ public class AjoutArbre extends AppCompatActivity {
         setContentView(R.layout.activity_ajout_arbre);
         spinnerNomArbre = findViewById(R.id.spinnerNomArbre);
         editTextNomArbre = findViewById(R.id.editTextNomArbre);
-
+        editTextNomPrenom = findViewById(R.id.editTextNomPrenom);
+        editTextAdresseMail = findViewById(R.id.editTextAdresseMail);
+        editTextPseudo = findViewById(R.id.editTextPseudo);
 
         //Location
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
@@ -88,6 +97,33 @@ public class AjoutArbre extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void saveData(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(TEXT_NOM_PRENOM, editTextNomPrenom.getText().toString());
+        editor.putString(TEXT_ADRESSE_MAIL, editTextAdresseMail.getText().toString());
+        editor.putString(TEXT_PSEUDO, editTextPseudo.getText().toString());
+
+        editor.apply();
+
+        Intent i = new Intent(this, MapsActivity.class);
+        Toast.makeText(this, "Alignement d'arbres enregistré !", Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+
+        String textNomPrenom = sharedPreferences.getString(TEXT_NOM_PRENOM, "");
+        String textAdresseMail = sharedPreferences.getString(TEXT_ADRESSE_MAIL, "");
+        String textPseudo = sharedPreferences.getString(TEXT_PSEUDO, "");
+
+        editTextNomPrenom.setText(textNomPrenom);
+        editTextAdresseMail.setText(textAdresseMail);
+        editTextPseudo.setText(textPseudo);
     }
 
 }
