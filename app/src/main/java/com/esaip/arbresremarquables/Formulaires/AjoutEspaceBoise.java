e1a1664bd33f915dff5cddb060b4908ca803afc3
MaximeDoreau <doreaumaxime@gmail.com>
2020-05-25 17:43:58 +0200

    Espace Boise : Verification Regexs V1

--- app/src/main/java/com/esaip/arbresremarquables/Formulaires/AjoutEspaceBoise.java
+++ app/src/main/java/com/esaip/arbresremarquables/Formulaires/AjoutEspaceBoise.java
@@ -3,14 +3,19 @@ package com.esaip.arbresremarquables.Formulaires;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.esaip.arbresremarquables.R;
+
+import java.util.regex.Pattern;
 
 public class AjoutEspaceBoise extends AppCompatActivity {
     //Variables pour la sauvegarde utilisateur
@@ -18,9 +23,12 @@ public class AjoutEspaceBoise extends AppCompatActivity {
     public static final String TEXT_NOM_PRENOM = "NOM_PRENOM";
     public static final String TEXT_ADRESSE_MAIL = "ADRESSE_MAIL";
     public static final String TEXT_PSEUDO = "PSEUDO";
-    private EditText editTextNomPrenom, editTextAdresseMail, editTextPseudo,editTextLatitude, editTextLongitude;
+    private EditText editTextNomPrenom, editTextAdresseMail, editTextPseudo,editTextLatitude, editTextLongitude, editTextAdresseArbre, editTextObservations ;
     private LinearLayout autre;
-    private CheckBox autreCheckBox;
+    private CheckBox autreCheckBox,checkBoxArbre,checkBoxArbuste,checkBoxHerbe,checkBoxEcureuil,checkBoxChauve,checkBoxCapricorne,checkBoxChouette,checkBoxPic,checkBoxRefuge,checkBoxIlot,checkBoxPaysager;
+    private Spinner spinnerTypeEspace, spinnerNbArbres, spinnerNbEspeces, spinnerEau, spinnerAbris, spinnerEclairage, spinnerOmbre, spinnerEntretien;
+    private Button buttonValider;
+    private String stringTextNomPrenom, stringTextPseudo, stringTextObservations, stringTextMail, stringTextAdresse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +36,33 @@ public class AjoutEspaceBoise extends AppCompatActivity {
         setContentView(R.layout.activity_ajout_espace_boise);
 
         //Setup - FindViewById
-        editTextNomPrenom = findViewById(R.id.editTextNomPrenom);
-        editTextAdresseMail = findViewById(R.id.editTextAdresseMail);
-        editTextPseudo = findViewById(R.id.editTextPseudo);
+        editTextNomPrenom = findViewById(R.id.editTextNomPrenomEsp);
+        editTextAdresseMail = findViewById(R.id.editTextAdresseMailEsp);
+        editTextPseudo = findViewById(R.id.editTextPseudoEsp);
+        editTextAdresseArbre = findViewById(R.id.editTextAdresseEsp);
+        editTextObservations = findViewById(R.id.editTextObservationEsp);
         autre = findViewById(R.id.editAutre);
         autreCheckBox = findViewById(R.id.checkautrebox);
+        checkBoxArbre = findViewById(R.id.checkBoxArbre);
+        checkBoxArbuste = findViewById(R.id.checkBoxArbuste);
+        checkBoxHerbe = findViewById(R.id.checkBoxHerbe);
+        checkBoxEcureuil = findViewById(R.id.checkBoxEcureuil);
+        checkBoxChauve = findViewById(R.id.checkBoxChauve);
+        checkBoxCapricorne = findViewById(R.id.checkBoxCapricorne);
+        checkBoxChouette = findViewById(R.id.checkBoxChouette);
+        checkBoxPic = findViewById(R.id.checkBoxPic);
+        checkBoxRefuge = findViewById(R.id.checkBoxRefuge);
+        checkBoxIlot = findViewById(R.id.checkBoxIlot);
+        checkBoxPaysager = findViewById(R.id.checkBoxPaysager);
+        spinnerTypeEspace = findViewById(R.id.spinnerEspaceEsp);
+        spinnerNbArbres = findViewById(R.id.spinnerNbArbresEsp);
+        spinnerNbEspeces = findViewById(R.id.spinnerNbEsp);
+        spinnerEau = findViewById(R.id.spinnerPtEau);
+        spinnerAbris = findViewById(R.id.spinnerAbris);
+        spinnerEclairage = findViewById(R.id.spinnerEclairage);
+        spinnerOmbre = findViewById(R.id.spinnerOmbre);
+        spinnerEntretien = findViewById(R.id.spinnerEntretien);
+        buttonValider = findViewById(R.id.buttonValiderEsp);
 
         //Location
         double lat=0;
@@ -43,12 +73,10 @@ public class AjoutEspaceBoise extends AppCompatActivity {
         if (bundle != null) {
             lat = bundle.getDouble("latitude");
             lon = bundle.getDouble("longitude");
-
             editTextLatitude = findViewById(R.id.editTextLatitude);
             editTextLongitude = findViewById(R.id.editTextLongitude);
             editTextLatitude.setText(String.valueOf(lat));
             editTextLongitude.setText(String.valueOf(lon));
-
         }
 
         autreCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -62,10 +90,66 @@ public class AjoutEspaceBoise extends AppCompatActivity {
             }
         });
 
+        buttonValider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringTextNomPrenom = editTextNomPrenom.getText().toString().trim();
+                stringTextPseudo = editTextPseudo.getText().toString().trim();
+                stringTextMail = editTextAdresseMail.getText().toString().trim();
+                stringTextAdresse = editTextAdresseArbre.getText().toString().trim();
+                stringTextObservations = editTextObservations.getText().toString().trim();
+                int count = 0;
+
+                if (!stringTextMail.isEmpty() && !checkPatternMail(stringTextMail)) {
+                    editTextAdresseMail.setError("Adresse mail non valide");
+                } else {
+                    count += 1;
+                }
+
+                if (stringTextNomPrenom.isEmpty()) {
+                    editTextNomPrenom.setError("Ce champ est obligatoire");
+                } else if (!checkPatternGeneral(stringTextNomPrenom)) {
+                    editTextNomPrenom.setError("Nom et prénom non valide");
+                } else {
+                    count += 1;
+                }
+
+                if (stringTextPseudo.isEmpty()) {
+                    editTextPseudo.setError("Ce champ est obligatoire");
+                } else if (!checkPatternPseudo(stringTextPseudo)) {
+                    editTextPseudo.setError("Pseudonyme non valide");
+                } else {
+                    count += 1;
+                }
+
+                if (stringTextAdresse.isEmpty()) {
+                    editTextAdresseArbre.setError("Ce champ est obligatoire");
+                } else if (!checkPatternAdresse(stringTextAdresse)) {
+                    editTextAdresseArbre.setError("Adresse non valide");
+                } else {
+                    count += 1;
+                }
+
+                if (!stringTextObservations.isEmpty() && !checkPatternObervations(stringTextObservations)) {
+                    editTextObservations.setError("Commentaires non valide");
+                } else {
+                    count += 1;
+                }
+
+                if (count == 5) {
+                    saveData();
+                    //finish();
+                    Toast.makeText(AjoutEspaceBoise.this, "Correct", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(AjoutEspaceBoise.this, "Champs incorrects ou manquants, veuillez remplir toutes les informations nécessaires", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
         loadData();
     }
 
-    public void saveData(View view) {
+    public void saveData() {
         openDialog();
 
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
@@ -92,5 +176,30 @@ public class AjoutEspaceBoise extends AppCompatActivity {
         editTextNomPrenom.setText(textNomPrenom);
         editTextAdresseMail.setText(textAdresseMail);
         editTextPseudo.setText(textPseudo);
+    }
+
+    private Boolean checkPatternMail(String txt){
+        Pattern MAIL = Pattern.compile("^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,3})+$");
+        return MAIL.matcher(txt).matches();
+    }
+
+    private Boolean checkPatternGeneral(String txt){
+        Pattern REG1 = Pattern.compile("^([A-Z][a-zâäèéêëîïôöûüñç ]+)(\\-?[A-Z][a-zâäèéêëîïôöûüñç ]+)*$");
+        return REG1.matcher(txt).matches();
+    }
+
+    private Boolean checkPatternPseudo(String txt){
+        Pattern PSEUDO = Pattern.compile("^([A-zâäèéêëîïôöûüñç\\-\\d ])+$");
+        return PSEUDO.matcher(txt).matches();
+    }
+
+    private Boolean checkPatternAdresse(String txt){
+        Pattern ADRESSE = Pattern.compile("^([A-Za-zâäèéêëîïôöûüñç\\-\\d ,])+[']?([A-Za-zâäèéêëîïôöûüñç\\-\\d ,])*$");
+        return ADRESSE.matcher(txt).matches();
+    }
+
+    private Boolean checkPatternObervations(String txt){
+        Pattern OBSERVATIONS = Pattern.compile("^(([A-Za-zâäàèéêëîïôöûüùñç\\-\\d ])+[']?([A-Za-zâäàèéêëîïôöûüùñç\\-\\d ])*([,\\.;/!:?()\\[\\]])*)+$");
+        return OBSERVATIONS.matcher(txt).matches();
     }
 }
